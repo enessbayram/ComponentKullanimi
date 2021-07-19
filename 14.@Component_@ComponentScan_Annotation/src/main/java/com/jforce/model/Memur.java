fd2b4efd61ee02ae48ceb6f0ab1f7d66ff1f1e1d
enes <81428286+enesbayyram@users.noreply.github.com>
2021-07-19 15:14:51 +0300

    meslek field eklenmesi

--- 14.@Component_@ComponentScan_Annotation/src/main/java/com/jforce/model/Memur.java
+++ 14.@Component_@ComponentScan_Annotation/src/main/java/com/jforce/model/Memur.java
@@ -3,6 +3,7 @@ package com.jforce.model;
 public class Memur {
 
 	private String isim;
+	private String meslek;
 
 	public String getIsim() {
 		return isim;
@@ -11,5 +12,14 @@ public class Memur {
 	public void setIsim(String isim) {
 		this.isim = isim;
 	}
+
+	public String getMeslek() {
+		return meslek;
+	}
+
+	public void setMeslek(String meslek) {
+		this.meslek = meslek;
+	}
+	
 	
 }
