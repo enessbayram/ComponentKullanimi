90fc140eb7709014a9839b6ad77198d108b8fd11
enes <81428286+enesbayyram@users.noreply.github.com>
2021-07-19 15:13:33 +0300

    memur sınıfı ekledim arkadaslar

---
+++ 14.@Component_@ComponentScan_Annotation/src/main/java/com/jforce/model/Memur.java
@@ -0,0 +1,15 @@
+package com.jforce.model;
+
+public class Memur {
+
+	private String isim;
+
+	public String getIsim() {
+		return isim;
+	}
+
+	public void setIsim(String isim) {
+		this.isim = isim;
+	}
+	
+}
